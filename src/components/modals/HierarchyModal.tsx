8eff2f637f31822c13d98c13a9dd5ac3d657b739
mrevanzak <mrevanzak@gmail.com>
2023-05-05 19:16:51 +0700

    feat(HierarchyModal): use custom node component

--- src/components/modals/HierarchyModal.tsx
+++ src/components/modals/HierarchyModal.tsx
@@ -1,6 +1,7 @@
 import { Modal } from '@mantine/core';
 import { useQuery } from '@tanstack/react-query';
-import Tree from 'react-d3-tree';
+import Link from 'next/link';
+import Tree, { CustomNodeElementProps } from 'react-d3-tree';
 import { ImSpinner2 } from 'react-icons/im';
 
 import getHierarchy from '@/services/company/getHierarchy';
@@ -10,6 +11,26 @@ type HierarchyModalProps = {
   close: () => void;
   companyId: string;
 };
+
+type RenderNodeProps = CustomNodeElementProps & {
+  close: () => void;
+  nodeDatum: CustomNodeElementProps['nodeDatum'] & {
+    id?: string;
+  };
+};
+
+function renderNode({ toggleNode, nodeDatum, close }: RenderNodeProps) {
+  return (
+    <g>
+      <circle r='15' onClick={toggleNode} />
+      <Link href={`/company/${nodeDatum.id}`} onClick={close}>
+        <text fill='black' strokeWidth='1' x='20'>
+          {nodeDatum.name}
+        </text>
+      </Link>
+    </g>
+  );
+}
 
 export default function HierarchyModal({
   opened,
@@ -36,6 +57,9 @@ export default function HierarchyModal({
             dimensions={{ width: 780, height: 384 }}
             translate={{ x: 390, y: 192 }}
             pathFunc='step'
+            renderCustomNodeElement={(rd3tProps) =>
+              renderNode({ ...rd3tProps, close })
+            }
           />
         )}
       </div>
